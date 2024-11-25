d8a4db900e1a584a2c0725915f0654e5d8281b50
Makarajyothi.Venturi <151708202+Kanakadurgaventuri@users.noreply.github.com>
2024-11-25 09:13:55 +0530

    Update Datatypes.java

--- Datatypes.java
+++ Datatypes.java
@@ -2,35 +2,25 @@ import java.io.*;
 import java.util.Scanner;
 class PrimitiveDataTypes
 	{
-		static byte b;
-		static short s;
 		static int i;
-		static long l;
-		static float f;
 		static double d;
-		static char c;
+		static short s;
 		static boolean a;
+		static float f;
+		static long l;
+		static char c;
+		static byte b;
 		public static void main(String args[])
 		{
-			Scanner obj=new Scanner(System.in);
-			System.out.print("Byte value: ");
-			b=obj.nextByte();
-			System.out.print("short value: ");
-			s=obj.nextShort();
-			System.out.print("int value: ");
-			i=obj.nextInt();
-			System.out.print("long value: ");
-			l=obj.nextLong();
-			System.out.print("float value: ");
-			f=obj.nextFloat();
-			System.out.print("double value: ");
-			d=obj.nextDouble();
-			System.out.print("char value: ");
-			c=obj.next().charAt(0);
-			System.out.print("boolean value (true/false): ");
-			a=obj.nextBoolean();
-			System.out.println("byteVar: "+b);
-			System.out.println("shortVar: "+s);
-			System.out.println("intVar: "+i);
-			System.out.println("longVar: "+l);
-			System.out.println("floatVar: "+f);
+			System.out.println("Default value for integer : "+i);
+			System.out.println("Default value for double : "+d);
+			System.out.println("Default value for short : "+s);
+			System.out.println("Default value for boolean : "+a);
+			System.out.println("Default value for float : "+f);
+			System.out.println("Default value for long : "+l);
+			System.out.println("Default value for char : "+c);
+			System.out.println("Default value for byte : "+b);
+
+
+		}
+}
